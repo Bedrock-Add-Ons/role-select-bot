4cba5f26eb03be8cbb12bdb5712f1895cd712cdb
Hatchibombotar <72224591+Hatchibombotar@users.noreply.github.com>
2022-01-26 18:13:06 +0000

    Created Bot Files.

---
+++ index.js
@@ -0,0 +1,58 @@
+const fs = require('fs')
+const { Client, Intents, MessageActionRow, MessageSelectMenu, Permissions } = require('discord.js')
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
+const config = require('./config.json')
+const roleConfig = require('./roles.json')
+
+client.once('ready', () => {
+    console.log('Ready!')
+})
+
+client.on("messageCreate", message => { 
+    if (!message.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) return;
+    if (message.content == "!createSelectRoles") {
+
+        const rows = []
+        for (selecConNum in roleConfig) {
+            const selectConfig = roleConfig[selecConNum]
+            const OptionsArray = []
+            for (rolenum in selectConfig.options) {
+                const role = selectConfig.options[rolenum]
+                OptionsArray.push({ "label": message.guild.roles.cache.get(role.role).name, "value": message.guild.roles.cache.get(role.role).id })
+                if (role.emoji) OptionsArray[rolenum].emoji = role.emoji
+            }
+
+            const minValues = selectConfig.min ? selectConfig.min : 0
+            const maxValues = selectConfig.max ? selectConfig.max : selectConfig.options.length
+
+            const Row = new MessageActionRow().addComponents(
+                new MessageSelectMenu()
+                    .addOptions(OptionsArray)
+                    .setCustomId(`${selecConNum}`)
+                    .setMinValues(minValues)
+                    .setMaxValues(maxValues)
+                    .setPlaceholder(selectConfig.name)
+            )
+            rows.push(Row)
+        }
+        message.channel.send({
+            content: fs.readFileSync("./message.txt") + " ",
+            components: rows
+        })
+    }
+
+})
+
+client.on('interactionCreate', interaction => {
+    if (!interaction.isSelectMenu()) return
+    const reactionRoles = roleConfig[interaction.customId].options.map(x => x.role)
+    const currentRoles = interaction.member._roles
+    const selectedRoles = interaction.values
+    const remove = reactionRoles.filter(x => currentRoles.includes(x))
+
+    if (remove.length > 0) for (role of remove) { interaction.member.roles.remove(role) }
+    if (selectedRoles.length > 0) for (role of selectedRoles) { interaction.member.roles.add(role) }
+    interaction.deferUpdate()
+})
+
+client.login(config.token)
---
+++ readme.md
@@ -0,0 +1,31 @@
+# Role Select Bot
+A drop-down based role select discord bot writen in discord.js.
+
+There are three files you should edit before starting the bot:
+
+- `config.json` - Add your bots token
+- `message.txt` - Replace with the message you want to send with the role dropdown.
+- `roles.json` - Put all the dropdowns in here (example below)
+
+## `roles.json`
+You should have a JSON array that looks something like the one below:
+
+```json
+[
+    {
+        "name": "My Roles",
+        "options": [
+            { "role": "933841152518545540", "emoji": "933841527858397225" },
+            { "role": "933841218033561600", "emoji": "933841527858397225" }
+        ],
+        "min": 0,
+        "max": 1
+    }
+]
+```
+
+`name` is the text you want to display.
+
+`options` are the roles that you want to be selectable, containing the role and emoji IDs
+
+`min` and `max` are omitable unless you want to limit the roles that can be chosen or removed.
